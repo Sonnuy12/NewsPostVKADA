55d77715303eb3d0b04f3baaf72e9aadcb92be11
Sonnuy12 <kazanskij516@gmail.com>
2024-12-09 13:04:24 +0300

    1) Настройка стека CoreData и структуры модели

---
+++ NewsPostVKADA/CoreData/CoreDataManager .swift
@@ -0,0 +1,35 @@
+//
+//  CoreDataWork.swift
+//  NewsPostVKADA
+//
+//  Created by сонный on 09.12.2024.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}

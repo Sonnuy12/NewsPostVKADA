59b3f062993f50a187f68008d0d251ab8d1ec4ba
Sonnuy12 <kazanskij516@gmail.com>
2024-12-15 14:36:32 +0300

    Создал экран авторизации и кнопку для VK API.

--- NewsPostVKADA/SceneDelegate.swift
+++ NewsPostVKADA/SceneDelegate.swift
@@ -13,13 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: scene)
         self.window?.makeKeyAndVisible()
-        self.window?.rootViewController = UINavigationController(rootViewController: TabBarController()) 
+        self.window?.rootViewController = UINavigationController(rootViewController: AuthorizationView())
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
--- NewsPostVKADA/ScreensAll/Authorization/AuthorizationView/AuthorizationView.swift
+++ NewsPostVKADA/ScreensAll/Authorization/AuthorizationView/AuthorizationView.swift
@@ -15,10 +15,23 @@ class AuthorizationView: UIViewController, AuthorizationViewProtocol {
     // MARK: - Properties
     var presenter: AuthorizationPresenterProtocol?
     
+    lazy var authorizationButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Войти", for: .normal)
+        button.backgroundColor = UIColor.black
+        button.tintColor = UIColor.white
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(authorizationButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    
     // MARK: - Func
     override func viewDidLoad() {
         super.viewDidLoad()
         createBackround()
+        view.addSubview(authorizationButton)
+        setupButtonConstraints()
     }
     
     func createBackround() {
@@ -28,5 +41,18 @@ class AuthorizationView: UIViewController, AuthorizationViewProtocol {
         self.view.addSubview(backgroundImageView)
         self.view.sendSubviewToBack(backgroundImageView)
     }
+    
+    private func setupButtonConstraints() {
+        authorizationButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            authorizationButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            authorizationButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            authorizationButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            authorizationButton.heightAnchor.constraint(equalToConstant: 51)
+        ])
+    }
+    // метод действия для кнопки
+    @objc func authorizationButtonTapped() {
+        print("Кнопка авторизации нажата!")
+    }
 }
-
